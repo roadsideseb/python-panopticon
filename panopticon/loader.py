748186577220b560340be0e13bd1034b1cabe751
Sebastian Vetter <seb@roadsi.de>
2016-03-21 16:36:05 -0700

    Add module loader for healthchecks

---
+++ panopticon/loader.py
@@ -0,0 +1,17 @@
+import structlog
+import importlib
+
+log = structlog.get_logger('panopticon.loader')
+
+
+def load_healthcheck_modules(package_names, module_name='healthchecks'):
+    for name in package_names:
+        healthcheck_module = '{name}.{module}'.format(name=name,
+                                                      module=module_name)
+
+        try:
+            importlib.import_module(healthcheck_module)
+        except ImportError:
+            log.debug('importing {} failed'.format(healthcheck_module))
+        else:
+            log.info('imported: {}'.format(healthcheck_module))
